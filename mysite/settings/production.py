8d5b556598c27e13c7bd03fd14772d7dbe488939
msabetta <mario.sabetta@outlook.it>
2022-03-31 13:03:22 +0200

    fix basedir_test production

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -3,10 +3,13 @@ from .default import *
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR_TEST = os.path.dirname(os.path.dirname(os.path.abspath("__file__"))) + "/mysite/"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR_TEST, 'db_test.sqlite3'),
     }
 }
 
