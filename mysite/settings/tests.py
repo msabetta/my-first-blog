a755e997dd6c0e54ccf0430989fda0564b1c9e7e
msabetta <mario.sabetta@outlook.it>
2022-03-31 12:51:01 +0200

    fix basedir

--- mysite/settings/tests.py
+++ mysite/settings/tests.py
@@ -3,6 +3,9 @@ from .default import *
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath("__file__")))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
