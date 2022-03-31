4e6751d1321c653a614dc63a29aa1c5136899b4e
msabetta <mario.sabetta@outlook.it>
2022-03-31 10:54:05 +0200

    fix production

---
+++ mysite/settings/production.py
@@ -0,0 +1,13 @@
+from .default import *
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
+    }
+}
+
+
