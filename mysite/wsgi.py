53d8e78c96b489bd5e89c82e51d7eaf3a3abef9e
msabetta <mario.sabetta@outlook.it>
2022-04-01 09:11:51 +0200

    changes django select settings module

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+select = 0
+
+if select is not 0:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+else:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings.production'
+
 
 application = get_wsgi_application()
